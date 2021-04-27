79dede817c5634712853f42ea3206b4eb233990f
Eva-ja <eva_1999@tut.by>
2021-04-28 02:51:35 +0300

    creat modal

--- js/main.js
+++ js/main.js
@@ -35,3 +35,32 @@ $(function() {
     }
 });
 });
+
+var modalButton = $("[data-toggle=modal]");
+var closeModalButton = $(".modal__close");
+modalButton.on("click", openModal);
+closeModalButton.on("click", closeModal);
+
+function openModal() {
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.addClass('modal__overlay--visible');
+    modalDialog.addClass('modal__dialog--visible')
+}
+function closeModal() {
+    event.preventDefault();
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass('modal__overlay--visible');
+    modalDialog.removeClass('modal__dialog--visible')
+}
+
+
+$(document).keyup(function(e) {
+  var modalOverlay = $(".modal__overlay");
+  var modalDialog = $(".modal__dialog");
+    if (e.keyCode === 27) { 
+      modalOverlay.removeClass("modal__overlay--visible");
+      modalDialog.removeClass("modal__dialog--visible");
+    }
+});
